1be37cad313ceb9a5dc00638f8522ec2d01f793f
Gyanaranjan Das <dasgyanaranjan835@gmail.com>
2026-02-03 23:47:22 +0530

    updated the error

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -3,25 +3,25 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const envVar = [
-    "PORT",
-    "NODE_ENV",
-    "MONGO_URI",
-    "JWT_SECRET",
-    "JWT_EXPIRES_IN",
-    "LOG_LEVEL",
-    "CORS_ORIGIN",
+    'PORT',
+    'NODE_ENV',
+    'MONGO_URI',
+    'JWT_ACCESS_SECRET',
+    'JWT_REFRESH_SECRET'
 ]
 
 envVar.forEach((key) => {
-    if(!process.env[key]){
+    if (!process.env[key]) {
         throw new Error(`Missing required environment variable: ${key}`);
     }
 })
 
 const env = {
-    port : process.env.PORT,
-    nodeEnv : process.env.NODE_ENV,
-    mongoUri : process.env.MONGO_URI,
+    PORT: process.env.PORT,
+    NODE_ENV: process.env.NODE_ENV,
+    MONGO_URI: process.env.MONGO_URI,
+    JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
+    JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET
 }
 
 export default env;
